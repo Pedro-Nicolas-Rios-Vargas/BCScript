8619d431bf0e3c89e8f428afb1d7b9639da20a0b
Ordep Soir <pv123124@gmail.com>
2021-10-27 03:52:16 -0500

    Socket Server modificado.
    Ahora el socket server tiene un tiempo de vida definido (60 segs - 1 min),
    esto con la finalidad de evitar saturación de sockets cuando se llama al
    método de la clase.
    Además, ahora el socket server que sea creado (más joven) tiene mayor
    prioridad que el más viejo de usar el puerto definido, por lo que, si
    existe un socket escuchando el puerto que vaya a ser usado por el nuevo
    socket este toma prioridad, dejando al socket viejo en estado de
    bloqueo.

--- server.py
+++ server.py
@@ -1,21 +1,42 @@
 import socket
 import codecs
 
-HOST = 'localhost'
-PORT = 8080
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.bind((HOST, PORT))
-    server.listen(1)
-    conn, addr = server.accept()
-    with conn:
-        print('Connected by', addr)
-        while conn:
-            data = conn.recv(4096)
-            if data:
-                conn.send(b'1')
-            if not data: break
-            print(codecs.decode(data))
-            pass
-        pass
-    pass
+class ServerSocket:
+
+    def __init__(self):
+        self.runServer = True
+
+    def initServer(self):
+        HOST = 'localhost'
+        PORT = 8080
+        barcode = ''
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            result = ''
+            # 60 segs of utility, after that the socket died.
+            server.settimeout(60)
+            # If another socket are bound to the same address that this socket
+            # gonna use, the old socket just go to BLOCKING MODE and let this
+            # socket use the address. 👍
+            server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server.bind((HOST, PORT))
+            server.listen(1)
+            conn, addr = server.accept()
+            with conn:
+                print('Connected by', addr)
+                while conn:
+                    data = conn.recv(4096)
+                    print(data)
+                    if data:
+                        conn.send(b'1')
+                    if not data:
+                        print('Socket server apagado.')
+                        break
+                    result = codecs.decode(data)
+                    barcode = result
+                    print(result)
+#            server.close()
+        return barcode
+
+    def disconnectServer(self):
+        self.runServer = False
