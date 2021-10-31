3c56ddb001ea5f7e4c61d5b8991978b7adb4d4fc
Ordep Soir <pv123124@gmail.com>
2021-10-30 20:35:33 -0500

    Se reforzo las opciones de conexión con el server
    Ahora el socket debe terminar su ejecución cuando un nuevo socket
    solicita el puerto al cual esta escuchando.

--- server.py
+++ server.py
@@ -12,30 +12,37 @@ class ServerSocket:
         PORT = 8080
         barcode = ''
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            print('\nIniciando el servidor socket..')
             result = ''
             # 60 segs of utility, after that the socket died.
-            server.settimeout(60)
+            server.settimeout(10)
             # If another socket are bound to the same address that this socket
             # gonna use, the old socket just go to BLOCKING MODE and let this
             # socket use the address. 👍
-            server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
             server.bind((HOST, PORT))
+            print(f'\nconectado al host: { HOST }')
+            print(f'conectado al puerto: { PORT }\n')
             server.listen(1)
-            conn, addr = server.accept()
-            with conn:
-                print('Connected by', addr)
-                while conn:
-                    data = conn.recv(4096)
-                    print(data)
-                    if data:
-                        conn.send(b'1')
-                    if not data:
-                        print('Socket server apagado.')
-                        break
-                    result = codecs.decode(data)
-                    barcode = result
-                    print(result)
-#            server.close()
+            try:
+                conn, addr = server.accept()
+                with conn:
+                    print('Conectado con: ', addr)
+                    while conn:
+                        data = conn.recv(4096)
+                        if data:
+                            conn.send(b'1')
+                        if not data:
+                            break
+                        result = codecs.decode(data)
+                        barcode = result
+                        print(f'Barcode: {result}')
+
+            except socket.timeout:
+                print('Socket server timeout')
+
+            print('Servidor socket apagado.\n')
+            server.close()
         return barcode
 
     def disconnectServer(self):
